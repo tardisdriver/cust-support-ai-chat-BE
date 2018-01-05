0363b0b8f844c3db7b91b8e90550df6f9123246a
tardisdriver <stark.tracy@gmail.com>
2018-01-05 15:45:52 -0600

    CORS Middleware

--- server.js
+++ server.js
@@ -1,17 +1,45 @@
+'use strict';
+
 const express = require('express');
 //const router = express.Router();
 //const bodyParser = require('body-parser');
 
+const STORE = {
+  "customers": [{
+    "name": "Bob Belcher",
+    "id": "654ZYX"
+  }, {
+    "name": "Linda Belcher",
+    "id": "901GHD"
+  }, {
+    "name": "Tina Belcher",
+    "id": "345HIJ"
+  }]
+}
+
 const app = express();
 
-//app.use(express.static('public'));
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE');
+  next();
+});
 
-app.get('/users', (req, res) => {
-    //find the service number in the database
-    //
+async function findCustomerById(id) {
+  const customers = STORE.customers;
+  return customers.find((cust) => id === cust.id);
+}
 
+app.get('/customers/:id', async (req, res) => {
+  const customer = await findCustomerById(req.params.id);
+  if (customer) {
+    res.json(customer);
+  } else {
+    res.sendStatus(404);
+  }
 });
 
 app.listen(process.env.PORT || 8080, () => {
-    console.log(`Server listening on port ${process.env.PORT || 8080}`);
+  console.log(`Server listening on port ${process.env.PORT || 8080}`);
 });
